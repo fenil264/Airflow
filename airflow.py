f6af18d8a0bc5464e7ca1cfa1ab9718ed6abd183
fenil264 <57947920+fenil264@users.noreply.github.com>
2023-09-22 16:42:59 +0530

    Update airflow.py

--- airflow.py
+++ airflow.py
@@ -6,19 +6,23 @@ from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
 import pandas as pd
 
-
+# Function to convert temperature from Kelvin to Fahrenheit
 def kelvin_to_fahrenheit(temp_in_kelvin):
     temp_in_fahrenheit = (temp_in_kelvin - 273.15) * (9/5) + 32
     return temp_in_fahrenheit
 
+# Function to transform and load weather data
 def transform_load_data(task_instance):
+    # Extract data from XCom
     data = task_instance.xcom_pull(task_ids="extract_weather_data")
+
+    # Extract relevant weather information
     city = data["name"]
     weather_description = data["weather"][0]['description']
-    temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp"])
-    feels_like_farenheit= kelvin_to_fahrenheit(data["main"]["feels_like"])
-    min_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_min"])
-    max_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_max"])
+    temp_fahrenheit = kelvin_to_fahrenheit(data["main"]["temp"])
+    feels_like_fahrenheit = kelvin_to_fahrenheit(data["main"]["feels_like"])
+    min_temp_fahrenheit = kelvin_to_fahrenheit(data["main"]["temp_min"])
+    max_temp_fahrenheit = kelvin_to_fahrenheit(data["main"]["temp_max"])
     pressure = data["main"]["pressure"]
     humidity = data["main"]["humidity"]
     wind_speed = data["wind"]["speed"]
@@ -26,31 +30,35 @@ def transform_load_data(task_instance):
     sunrise_time = datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
     sunset_time = datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
 
-    transformed_data = {"City": city,
-                        "Description": weather_description,
-                        "Temperature (F)": temp_farenheit,
-                        "Feels Like (F)": feels_like_farenheit,
-                        "Minimun Temp (F)":min_temp_farenheit,
-                        "Maximum Temp (F)": max_temp_farenheit,
-                        "Pressure": pressure,
-                        "Humidty": humidity,
-                        "Wind Speed": wind_speed,
-                        "Time of Record": time_of_record,
-                        "Sunrise (Local Time)":sunrise_time,
-                        "Sunset (Local Time)": sunset_time                        
-                        }
+    # Create a dictionary with transformed data
+    transformed_data = {
+        "City": city,
+        "Description": weather_description,
+        "Temperature (F)": temp_fahrenheit,
+        "Feels Like (F)": feels_like_fahrenheit,
+        "Minimum Temp (F)": min_temp_fahrenheit,
+        "Maximum Temp (F)": max_temp_fahrenheit,
+        "Pressure": pressure,
+        "Humidity": humidity,
+        "Wind Speed": wind_speed,
+        "Time of Record": time_of_record,
+        "Sunrise (Local Time)": sunrise_time,
+        "Sunset (Local Time)": sunset_time
+    }
+
+    # Create a DataFrame with the transformed data
     transformed_data_list = [transformed_data]
     df_data = pd.DataFrame(transformed_data_list)
 
+    # Generate a timestamp for the file name
     now = datetime.now()
     dt_string = now.strftime("%d%m%Y%H%M%S")
-    dt_string = 'current_weather_data_ahmedabad_' + dt_string
-    df_data.to_csv(f"s3://weatherapiairflow-yaml{dt_string}.csv", index=False)
+    file_name = 'current_weather_data_ahmedabad_' + dt_string
 
+    # Save the DataFrame to an S3 bucket
+    df_data.to_csv(f"s3://weatherapiairflow-yaml/{file_name}.csv", index=False)
 
-
-
-
+# Define default DAG arguments
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -62,33 +70,34 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-
-
+# Define the DAG
 with DAG('weather_dag',
         default_args=default_args,
-        schedule_interval = '@daily',
+        schedule_interval='@daily',
         catchup=False) as dag:
 
-
+        # Check if the weather API is ready
         is_weather_api_ready = HttpSensor(
-        task_id ='is_weather_api_ready',
-        http_conn_id='weathermap_api',
-        endpoint='/data/2.5/weather?q=ahmedabad&appid=XXXXXXXXXXXXXXXXXXXXXXXXX'
+            task_id='is_weather_api_ready',
+            http_conn_id='weathermap_api',
+            endpoint='/data/2.5/weather?q=ahmedabad&appid=XXXXXXXXXXXXXXXXXXXXXXXXX'
         )
 
+        # Extract weather data from the API
         extract_weather_data = SimpleHttpOperator(
-        task_id = 'extract_weather_data',
-        http_conn_id = 'weathermap_api',
-        endpoint='/data/2.5/weather?q=XXXXXXXXXXXXXXXXXXXXXXXXXX',
-        method = 'GET',
-        response_filter= lambda r: json.loads(r.text),
-        log_response=True
+            task_id='extract_weather_data',
+            http_conn_id='weathermap_api',
+            endpoint='/data/2.5/weather?q=XXXXXXXXXXXXXXXXXXXXXXXXXX',
+            method='GET',
+            response_filter=lambda r: json.loads(r.text),
+            log_response=True
         )
 
+        # Transform and load weather data
         transform_load_weather_data = PythonOperator(
-        task_id= 'transform_load_weather_data',
-        python_callable=transform_load_data
+            task_id='transform_load_weather_data',
+            python_callable=transform_load_data
         )
 
-
+        # Define task dependencies
         is_weather_api_ready >> extract_weather_data >> transform_load_weather_data
